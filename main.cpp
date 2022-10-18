4e59f92b376180dd0bad111934277c5ed8b33bf9
umriatihalimu <yumsesai21@gmail.com>
2022-10-18 20:54:37 +0700

    E1E120052_UMRIATI_(LINE 225-294)

--- main.cpp
+++ main.cpp
@@ -220,3 +220,77 @@
     }
 
 //line akhir naila
+
+
+//E1E120052_UMRIATI
+//line 225-294
+void bilah (float r_inner,float r_outer,float tebal,int batang)
+    {
+    float i;
+    glPushMatrix();
+    glTranslatef(0.0,0.0,-tebal/4);
+    cylinder(r_inner,r_inner,tebal);
+    glTranslatef(0.0,0.0,tebal/2);
+    glRotatef(90,0.0,1.0,0.0);
+    for(i=0;i<batang;i++)
+        {
+        glTranslatef(0.0,0.0,r_inner);
+        glRotatef(315,0.0,0.0,1.0);
+        blok(0.5,r_inner*4.5,(r_outer-r_inner+(r_inner/4))*2);
+        glRotatef(45,0.0,0.0,1.0);
+        glTranslatef(0.0,0.0,-r_inner);
+        glRotatef(360/batang,1.0,0.0,0.0);
+        }
+    glPopMatrix();
+    }
+
+
+    void blok(float tebal,int ratiol,int ratiop)
+    {
+    float i,j;
+    glPushMatrix();
+        for(i=0;i<ratiop;i++)
+        {
+        glTranslatef(-(ratiol+1)*tebal/2,0.0,0.0);
+        for(j=0;j<ratiol;j++)
+            {
+            glTranslatef(tebal,0.0,0.0);
+            glutSolidCube(tebal); // membuat kubus
+            }
+        glTranslatef(-(ratiol-1)*tebal/2,0.0,tebal);
+        }
+    glPopMatrix();
+    }
+
+    //efek keyboard
+    void keyboard_s(int key,int x,int y)
+    {
+        if (rotation_y_plus !=0)
+        direction=(rotation_y_plus/abs(rotation_y_plus));
+        else
+        direction=-1;
+        switch(key)
+        {
+        case GLUT_KEY_UP:// menaikan kipas
+            rotation_y_plus++;
+            break;
+        case GLUT_KEY_DOWN:// menurunkan kipas
+            rotation_y_plus--;
+            break;
+        case GLUT_KEY_END:// stop kipas
+            rotation_y_plus=0;
+            speed1=false;
+            pressplus1=0;
+            speed2=false;
+            pressplus2=0;
+            speed3=false;
+            pressplus3=0;
+            speed4=false;
+            pressplus4=0;
+            pressplus5=180;
+            Toleh=false;
+            break;
+
+    // Line akhir umriati
+
+
